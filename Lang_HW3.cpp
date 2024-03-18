f29d2a870d280e4fe85ca45fe2b4868c672caf0d
shimsterx <157174526+shimsterx@users.noreply.github.com>
2024-03-18 17:20:48 -0500

    Add files via upload

---
+++ Lang_HW3.cpp
@@ -0,0 +1,26 @@
+int main()
+int temFeb1, tempfeb2, tempfeb3
+(
+
+starting Cityname
+
+cout << "Welcome to Weather at Home!" << endl 
+cout << "What city are you from? ";
+cin >> Cityname;
+
+Cout << "What was the temperature on February 1? 66 "; 
+cin >> temFeb1;
+
+Cout << "What was the temperature on February 28? 66 "; 
+cin >> tempfeb2;
+
+tempdiff = tempfeb2 - temFeb1;
+
+cout << "The temperature difference in " << cityname 
+        << " from February 1 to 28 was " << tempdiff << "degrees" endl;
+        cout << "Thanks for using Weather at Home " endl;
+
+
+
+
+)
